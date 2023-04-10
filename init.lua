2b1a521dca74e2ff011ec9c8774352ceb5157f30
Kelvin Dealca <kelvin.dealca@gmail.com>
2023-04-10 07:30:52 -0700

    Add saving in insert mode and more ergonomic buffer switching

--- init.lua
+++ init.lua
@@ -238,6 +238,8 @@ vim.o.listchars = "trail:~,tab:>-,nbsp:␣"
 vim.o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 -- [[ Basic Keymaps ]]
 vim.keymap.set('n', '<C-s>', ':wall<CR>')
+vim.keymap.set('i', '<C-s>', '<Esc>:wall<CR>')
+vim.keymap.set('n', '<C-j>', '<C-^>')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzz')
